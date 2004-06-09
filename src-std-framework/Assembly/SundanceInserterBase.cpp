6c5e0ec9668918680d1a15e941f46508d2890411
Kevin R. Long <kevin.long@ttu.edu>
2004-06-09 20:59:39 +0000

    neglected to add these files

---
+++ src-std-framework/Assembly/SundanceInserterBase.cpp
@@ -0,0 +1,18 @@
+/* @HEADER@ */
+/* @HEADER@ */
+
+#include "SundanceInserterBase.hpp"
+#include "SundanceOut.hpp"
+#include "SundanceTabs.hpp"
+
+using namespace SundanceStdFwk;
+using namespace SundanceStdFwk::Internal;
+using namespace SundanceCore;
+using namespace SundanceCore::Internal;
+using namespace SundanceStdMesh;
+using namespace SundanceStdMesh::Internal;
+using namespace SundanceUtils;
+using namespace Teuchos;
+using namespace TSFExtended;
+
+
