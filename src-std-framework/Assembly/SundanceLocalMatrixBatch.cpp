8c926ca92ac5f7f1949f9c65451c23beb1b0f10b
Kevin R. Long <kevin.long@ttu.edu>
2004-06-09 20:58:14 +0000

    need to add LocalMatrixBatch

---
+++ src-std-framework/Assembly/SundanceLocalMatrixBatch.cpp
@@ -0,0 +1,14 @@
+/* @HEADER@ */
+/* @HEADER@ */
+
+#include "SundanceLocalMatrixBatch.hpp"
+#include "SundanceOut.hpp"
+#include "SundanceTabs.hpp"
+
+
+using namespace SundanceStdFwk;
+using namespace SundanceStdFwk::Internal;
+
+
+
+void LocalMatrixBatch::blah() {;}
---
+++ src-std-framework/Assembly/SundanceLocalMatrixBatch.hpp
@@ -0,0 +1,33 @@
+/* @HEADER@ */
+/* @HEADER@ */
+
+#ifndef SUNDANCE_LOCALMATRIXBATCH_H
+#define SUNDANCE_LOCALMATRIXBATCH_H
+
+#include "SundanceDefs.hpp"
+#include "SundanceDOFMapBuilder.hpp"
+
+#ifndef DOXYGEN_DEVELOPER_ONLY
+
+namespace SundanceStdFwk
+{
+  namespace Internal
+  {
+    /** 
+     *     
+     */
+    class LocalMatrixBatch
+    {
+    public:
+      /** Empty ctor */
+      LocalMatrixBatch(){;}
+
+      /** */
+      void blah() ;
+    };
+  }
+}
+
+#endif  /* DOXYGEN_DEVELOPER_ONLY */
+
+#endif
