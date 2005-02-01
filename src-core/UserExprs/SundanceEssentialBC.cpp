a6557c2efd309b8cbdaf1bf9591a04c9fbff3591
Stephen B. Margolis <margoli@sandia.gov>
2005-02-01 00:51:55 +0000

    *** empty log message ***

--- src-core/UserExprs/SundanceEssentialBC.cpp
+++ src-core/UserExprs/SundanceEssentialBC.cpp
@@ -3,6 +3,7 @@
 
 #include "SundanceEssentialBC.hpp"
 #include "SundanceSumOfBCs.hpp"
+#include "SundanceZeroExpr.hpp"
 
 using namespace SundanceCore;
 using namespace SundanceUtils;
@@ -15,6 +16,12 @@ using namespace SundanceCore::Internal;
 Expr SundanceCore::EssentialBC(const Handle<CellFilterStub>& domain,
                                const Expr& integrand)
 {
+  const ZeroExpr* z = dynamic_cast<const ZeroExpr*>(integrand.ptr().get());
+  const ConstantExpr* c = dynamic_cast<const ConstantExpr*>(integrand.ptr().get());
+  if (z != 0 || (c != 0 && c->value()==0.0))
+    {
+      return integrand;
+    }
   RefCountPtr<QuadratureFamilyStub> quad = rcp(new QuadratureFamilyStub(0));
   return new SumOfBCs(domain.ptr(), integrand, quad);
 }
@@ -23,5 +30,11 @@ Expr SundanceCore::EssentialBC(const Handle<CellFilterStub>& domain,
                                const Expr& integrand,
                                const Handle<QuadratureFamilyStub>& quad)
 {
+  const ZeroExpr* z = dynamic_cast<const ZeroExpr*>(integrand.ptr().get());
+  const ConstantExpr* c = dynamic_cast<const ConstantExpr*>(integrand.ptr().get());
+  if (z != 0 || (c != 0 && c->value()==0.0))
+    {
+      return integrand;
+    }
   return new SumOfBCs(domain.ptr(), integrand, quad.ptr());
 }
