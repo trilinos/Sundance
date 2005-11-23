4e67aced6615ff901138440557671fc7a292a177
Paul T. Boggs <ptboggs@sandia.gov>
2005-11-23 00:30:58 +0000

    Adding element access for block vectors Adding vector testing
    ----------------------------------------------------------------------
     Enter Log.  Lines beginning with `' are removed automatically
      Committing in .    Modified Files:  Makefile.in
    aclocal.m4 configure configure.ac  autom4te.cache/output.1
    autom4te.cache/requests  autom4te.cache/traces.1
    examples-tutorial/Makefile.in  src-core/Makefile.in
    src-solvers/Makefile.am  src-solvers/Makefile.in
    src-solvers/Vectors/TSFProductVectorDecl.hpp
    src-solvers/Vectors/TSFProductVectorImpl.hpp
    src-solvers/Vectors/TSFVectorDecl.hpp
    src-solvers/Vectors/TSFVectorImpl.hpp
    src-solvers/Vectors/TSFVectorSpaceDecl.hpp
    src-solvers/Vectors/TSFVectorSpaceImpl.hpp
    src-std-framework/Makefile.in src-std-mesh/Makefile.in
    src-utils/Makefile.in src-xml/Makefile.in  tests-core/Makefile.in
    tests-core/SymbolicTests/Makefile.in  tests-solvers/Makefile.am
    tests-solvers/Makefile.in  tests-solvers/OperatorTests/Makefile.in
     tests-std-framework/Makefile.in
    tests-std-framework/Assembly/Makefile.in
    tests-std-framework/HomogeneousDOFMap/Makefile.in
    tests-std-framework/Problem/Makefile.in
    tests-std-mesh/Makefile.in tests-std-mesh/Readers/Makefile.in
    tests-std-mesh/Simple/Makefile.in tests-utils/Makefile.in
    tests-utils/Functors/Makefile.in tests-utils/Maps/Makefile.in
    Added Files:  src-solvers/Testing/TSFVectorTester.hpp
    tests-solvers/VectorTests/Makefile.am
    tests-solvers/VectorTests/Makefile.in
    tests-solvers/VectorTests/VectorTest.cpp
    ----------------------------------------------------------------------

---
+++ tests-solvers/VectorTests/VectorTest.cpp
@@ -0,0 +1,81 @@
+//@HEADER
+// ***********************************************************************
+// 
+//           TSFExtended: Trilinos Solver Framework Extended
+//                 Copyright (2004) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//  
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov) 
+// 
+// ***********************************************************************
+//@HEADER
+
+#include "Teuchos_MPISession.hpp"
+#include "TSFVector.hpp"
+#include "TSFLinearCombination.hpp"
+#include "TSFVectorType.hpp"
+#include "TSFVectorSpace.hpp"
+#include "TSFEpetraVectorType.hpp"
+#include "Teuchos_Time.hpp"
+#include "TSFVectorTester.hpp"
+
+using namespace Teuchos;
+using namespace TSFExtended;
+using namespace TSFExtendedOps;
+
+
+int main(int argc, void *argv[]) 
+{
+  try
+    {
+      int verbosity = 2;
+
+      MPISession::init(&argc, &argv);
+
+      VectorType<double> type = new EpetraVectorType();
+
+      int n = 4;
+
+      int rank = MPISession::getRank();
+      int nProc = MPISession::getNProc();
+
+      int dimension = nProc*n;
+      int low = n*rank;
+      int high = n*(rank+1);
+      std::vector<int> localRows(n);
+      for (int i=0; i<n; i++)
+        {
+          localRows[i] = low + i;
+        }
+
+      VectorSpace<double> space = type.createSpace(dimension, n, 
+                                                   &(localRows[0]));
+
+      VectorTester<double> tester(space, TestSpecifier<double>(true, 1.0e-13, 1.0e-10));
+
+      tester.runAllTests();
+      
+    }
+  catch(std::exception& e)
+    {
+      cerr << "Caught exception: " << e.what() << endl;
+    }
+  MPISession::finalize();
+}
+
