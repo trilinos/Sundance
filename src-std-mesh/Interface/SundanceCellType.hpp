b6c1c50c580926ef16d5fded1821d09714f0f4aa
Bart G. Van Bloemen Waanders <bartv@sandia.gov>
2007-01-02 20:39:26 +0000

    Ross added some documentation to these files.

--- src-std-mesh/Interface/SundanceCellType.hpp
+++ src-std-mesh/Interface/SundanceCellType.hpp
@@ -35,34 +35,89 @@
 
 #include "SundanceDefs.hpp"
 
-namespace SundanceStdMesh
+namespace SundanceStdMesh {
+
+/** \defgroup Sundance_CellType_grp Cell Type Description
+ */
+
+/** \brief Enumeration of specific 1D, 2D, and 3D cell types.
+ *
+ * See the implementation of the nonmember functions <tt>dimension()</tt>,
+ * <tt>numFacets()</tt>, and <tt>facetType()</tt> a full description of what
+ * these cell types are.
+ *
+ * ToDo: Consider making CellType an abstract base class so that any type of
+ * cell can be implemented?
+ *
+ * \ingroup Sundance_CellType_grp
+ */
+enum CellType {
+  NullCell        ///< No cell specified
+  ,PointCell      ///< 0D vertex cell
+  ,LineCell       ///< 1D line, or edge, cell
+  ,TriangleCell   ///< 2D triangle
+  ,TetCell        ///< 3D tetrahedral cell
+  ,QuadCell       ///< 2D quadrilateral cell
+  ,BrickCell      ///< 3D "brick" cell
+  ,PrismCell      ///< 3D prism cell
+};
+
+/** \brief Return a string representation of the cell type.
+ *
+ * \ingroup Sundance_CellType_grp
+ */
+string toString(const CellType& c) ;
+
+/** \brief Return the dimension of the cell type.
+ *
+ * \ingroup Sundance_CellType_grp
+ */
+int dimension(const CellType& c) ;
+
+/** \brief Return the number of faces of a given facet dimension for a cell type.
+ *
+ * <b>Preconditions:</b><ul>
+ * <li><tt>facetDim <= dimension(c)</tt>
+ * </ul>
+ *
+ * \returns -1 for null cells and point cells
+ *
+ * \ingroup Sundance_CellType_grp
+ */
+int numFacets(const CellType& c, int facetDim);
+
+/** \return Return the type of facet of a given facet dimension and a
+ * particualar facet.
+ *
+ * \param  c
+ *           [in] Cell type
+ * \param  facetDim
+ *           [in] The dimension of the facet type requested
+ * \param  facetIndex
+ *           [in] The particualar index of the facet as defined
+ *           by the convension of the cell type (which is actually
+ *           partially defined in this function).
+ *
+ * <b>Preconditions:</b><ul>
+ * <li><tt>facetDim <= dimension(c)</tt>
+ * <li><tt>0 <= facetIndex < numFacets(c,facetDim)</tt>
+ * </ul>
+ *
+ * \ingroup Sundance_CellType_grp
+ */
+CellType facetType(const CellType& c, int facetDim, int facetIndex);
+
+/** \brief output stream operator for <tt>CellType</tt>.
+ *
+ * \ingroup Sundance_CellType_grp
+ */
+inline ostream& operator<<(ostream& os, const CellType& c)
 {
-  /** */
-  enum CellType {NullCell, PointCell, LineCell, TriangleCell, 
-                         TetCell, QuadCell, BrickCell, PrismCell};
-
-  /** \relates CellType */
-  string toString(const CellType& c) ;
-
-  /** \relates CellType */
-  int dimension(const CellType& c) ;
-
-  /** \relates CellType */
-  int numFacets(const CellType& c, int facetDim);
-
-  /** \relates CellType */
-  CellType facetType(const CellType& c, int facetDim, int facetIndex);
-
-
-  /** \relates CellType */
-  inline ostream& operator<<(ostream& os, const CellType& c)
-  {
-    os << toString(c);
-    return os;
-  }
-
-  
+  os << toString(c);
+  return os;
 }
+  
+} // namespace SundanceStdMesh
 
 #endif  /* DOXYGEN_DEVELOPER_ONLY */   
 
