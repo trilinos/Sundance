4213f56d6d83078cf0c5f34196ce20ff752b38e2
Kevin Long <kevin.long@ttu.edu>
2011-03-25 14:50:20 -0500

    Fixed a sign error in the transient nonlinear example. Extending documentation for that example.

--- examples-tutorial/c++/transientNonlinear1D.cpp
+++ examples-tutorial/c++/transientNonlinear1D.cpp
@@ -66,12 +66,15 @@ int main(int argc, char** argv)
 
       double epsilon = 0.25;
       /* The initial profile is u(x,0)=1 + epsilon x^2. 
-       * Project this onto a discrete function */
+       * Project this onto a discrete function. This discrete function, uPrev, will represent
+       * the initial conditions but will also be re-used as the starting value for
+       * each timestep */
+      Expr uStart = 1.0 + epsilon*x*x;
       DiscreteSpace discSpace(mesh, bas, vecType);
       L2Projector projector(discSpace, 1.0 + epsilon*x*x);
       Expr uPrev = projector.project();
 
-      Expr uStart = copyDiscreteFunction(uPrev, "uStart");
+      /* We need another discrete function for the current Newton approximation */
       Expr uNewt = copyDiscreteFunction(uPrev, "uNewt");
 
       /* We need a quadrature rule for doing the integrations */
@@ -79,6 +82,9 @@ int main(int argc, char** argv)
 
       int nSteps = 32;
       double dt = 1.0/((double) nSteps);
+      /* Represent the time variable as a parameter expression, NOT as
+       * a double variable. The reason is that we need to be able to update
+       * the time value without rebuilding expressions. */
       Expr t = new Sundance::Parameter(0.0);
       Expr tPrev = new Sundance::Parameter(0.0);
 
@@ -86,17 +92,19 @@ int main(int argc, char** argv)
       Expr eqn = Integral(interior, v*(u-uPrev) 
         + dt/2.0*(dx*v)*((dx*pow(u, 4.0))+(dx*pow(uPrev, 4.0)))
         - dt/2.0*v*(force(epsilon, x, t)+force(epsilon, x, tPrev)), quad); 
-      /* Define the Dirichlet BC */
-      Expr bc = EssentialBC(leftPoint, v*(u-1.0), quad) 
-        + EssentialBC(rightPoint, v*(u - 1.0 - epsilon*cos(2.0*pi*t)),quad);
+      /* Define the Dirichlet BC on the right side of the domain */
+      Expr bc = EssentialBC(rightPoint, v*(u - 1.0 - epsilon*cos(2.0*pi*t)),quad);
 
-      /* We can now set up the linear problem! */
+      /* We can now set up the nonlinear problem! */
       NonlinearProblem prob(mesh, eqn, bc, v, u, uNewt, vecType); 
 
+      /* Create a linear solver to be used for the linear solve
+       * at each Newton step */
       LinearSolver<double> linSolver 
         = LinearSolverBuilder::createSolver("amesos.xml");
 
-
+      
+      /* Allocate objects for the Jacobian, residual, and Newton step */
       LinearOperator<double> J = prob.allocateJacobian();
       Vector<double> resid = J.range().createMember();
       Vector<double> newtonStep = J.domain().createMember();
@@ -104,6 +112,7 @@ int main(int argc, char** argv)
       int maxNewtIters = 10;
       double newtTol = 1.0e-12;
 
+      /* Wrtie the initial conditions */
       FieldWriter w = new MatlabWriter("transientNonlinear1D-0.dat"); 
       w.addMesh(mesh);
       w.addField("u", new ExprFieldWrapper(uPrev[0]));
@@ -113,6 +122,7 @@ int main(int argc, char** argv)
       /* loop over timesteps */
       for (int i=0; i<nSteps; i++)
       {
+        /* Set the times t_i and t_{i+1} */
         Out::root() << "timestep #" << i << endl;
         t.setParameterValue((i+1)*dt);
         tPrev.setParameterValue(i*dt);
@@ -125,11 +135,11 @@ int main(int argc, char** argv)
           prob.setInitialGuess(uNewt);
           prob.computeJacobianAndFunction(J, resid);
           SolverState<double> solveState 
-            = linSolver.solve(J, resid, newtonStep);
+            = linSolver.solve(J, -1.0*resid, newtonStep);
           TEST_FOR_EXCEPTION(solveState.finalState() != SolveConverged,
             std::runtime_error,
             "linear solve failed!");
-          addVecToDiscreteFunction(uNewt, -1.0*newtonStep);
+          addVecToDiscreteFunction(uNewt, newtonStep);
           double newtStepNorm = newtonStep.norm2();
           Out::root() << "|newt step| = " << newtStepNorm << endl;
           if (newtStepNorm < newtTol) 
